b70dfc20756b11974995b12410132ad494ace478
Sudhir Mishra <sudhirxps@gmail.com>
2020-03-10 13:09:30 +0530

    Add data using JS

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var data = {
+        word: "adage",
+        meaning: "a proverb or short statement expressing a general truth."
+    };
+    $('#word').html(data.word);
+    $('#meaning').html(data.meaning);
+})
